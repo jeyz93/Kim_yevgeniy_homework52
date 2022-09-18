e20957acbc373202e9ee9ce78bd842b5928bbd13
Kim Yevgeniy <jeyz9@mail.ru>
2022-09-19 01:23:50 +0600

    Создана вью, для забора информации из баз данных

--- webapp/views.py
+++ webapp/views.py
@@ -1,10 +1,20 @@
 from django.core.handlers.wsgi import WSGIRequest
 from django.shortcuts import render
+from webapp.models import Tasks
+
+
+# def index_view(request: WSGIRequest):
+#     # articles = Article.objects.all()
+#     # context = {
+#     #     'articles' : articles
+#     # }
+#     return render(request, 'index.html')
+#
 
 
 def index_view(request: WSGIRequest):
-    # articles = Article.objects.all()
-    # context = {
-    #     'articles' : articles
-    # }
-    return render(request, 'index.html')
+    tasks = Tasks.objects.all()
+    context = {
+        'tasks': tasks
+    }
+    return render(request, 'index.html', context)
