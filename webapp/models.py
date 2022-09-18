f1b82edad1df8d63b2fc62debdf084fc98c7a035
Kim Yevgeniy <jeyz9@mail.ru>
2022-09-19 00:40:44 +0600

    Создана модель задач описаны 3поля задач

--- webapp/models.py
+++ webapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Tasks(models.Model):
+    description = models.CharField(verbose_name="Описание", max_length=200, null=False, blank=False)
+    status = models.CharField(verbose_name="Статус", max_length=100, null=False, blank=False,default="No name")
+    data_do = models.DateTimeField(verbose_name="Дата выполненияя", auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.description} - {self.status}"
